6767abdd9ad8b9622f0a3cb6aab07eb9a9693303
SonicBSV <sonicbsv@gmail.com>
2020-12-27 15:23:53 +0300

    Pixel Dust CAF Q

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pixeldust_markw.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixeldust_markw-userdebug
---
+++ pixeldust_markw.mk
@@ -0,0 +1,57 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+# Inherit some common stuff.
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit from Markw device
+$(call inherit-product,  device/xiaomi/markw/device.mk)
+
+# Google Apps
+#$(call inherit-product, vendor/gapps/gapps.mk)
+
+# Boot animation
+TARGET_ARCH := arm64
+BOOTANIMATION := 1080
+
+# Disable some debuggings
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_PACKAGES_DEBUG := false
+PRODUCT_PACKAGES_DEBUG_ASAN := false
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_NAME := pixeldust_markw
+BOARD_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := markw
+PRODUCT_MODEL := Redmi 4 Prime
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := markw
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.2.0.MBEMIXM release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.2.0.MBEMIXM:user/release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# PD platform includes
+include vendor/pixeldust/configs/pixeldust_phone.mk
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
+
+#PRODUCT_PACKAGES += \
+    Mms \
+    Messaging
