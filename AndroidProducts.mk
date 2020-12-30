1476f0c76f792116b619b20c15535e994fc278e9
SonicBSV <sonicbsv@gmail.com>
2020-12-30 06:41:28 +0300

    ConquerOS CAF Q

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/conquer_markw.mk
+
+COMMON_LUNCH_CHOICES := \
+    conquer_markw-userdebug
---
+++ conquer_markw.mk
@@ -0,0 +1,39 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+# Inherit some common stuff.
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+$(call inherit-product, vendor/conquer/config/common.mk)
+
+# Inherit from Markw device
+$(call inherit-product,  device/xiaomi/markw/device.mk)
+
+# Boot animation
+TARGET_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_NAME := conquer_markw
+BOARD_VENDOR := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := markw
+PRODUCT_MODEL := Redmi 4 Prime
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := markw
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V10.2.2.0.MBEMIXM release-keys" \
+    DEVICE_MAINTAINERS="SonicBSV"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V10.2.2.0.MBEMIXM:user/release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
